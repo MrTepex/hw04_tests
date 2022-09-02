614070cb2fc6f15ae67e1b11c250bded9de77168
Michael Terekhov <bardabary@gmail.com>
2022-09-02 15:15:13 +0300

    Кастом 403, 404, исправления в header.html

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'sorl.thumbnail',
 ]
 
 MIDDLEWARE = [
@@ -113,3 +114,5 @@ LOGIN_REDIRECT_URL = 'posts:index'
 
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
+
+CSRF_FAILURE_VIEW = 'core.views.csrf_failure'
